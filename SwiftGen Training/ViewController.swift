3d376ec1c841949fb6b54d682c6433b099347c28
Tyshkan4ik <001vat@gmail.com>
2023-09-19 17:12:18 +0300

    start project

---
+++ SwiftGen Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGen Training
+//
+//  Created by Виталий Троицкий on 19.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
