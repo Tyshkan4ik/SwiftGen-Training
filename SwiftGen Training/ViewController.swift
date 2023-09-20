fe941921f47f037803f14866773b3496cb328380
Tyshkan4ik <001vat@gmail.com>
2023-09-20 16:39:22 +0300

    added localizable in Russian

--- SwiftGen Training/ViewController.swift
+++ SwiftGen Training/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = L10n.welcom
+        label.font = UIFont.systemFont(ofSize: 30)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Asset.Colors.myColors.color
@@ -25,7 +35,7 @@ class ViewController: UIViewController {
 
     private func setupElements() {
         view.addSubview(imageView)
-//        view.addSubview(label)
+        view.addSubview(label)
     }
     
     private func setupConstraints() {
@@ -35,10 +45,9 @@ class ViewController: UIViewController {
             imageView.widthAnchor.constraint(equalToConstant: 300),
             imageView.heightAnchor.constraint(equalToConstant: 300),
             
-//            label.bottomAnchor.constraint(equalTo: imageView.topAnchor, constant: -50),
-//            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            label.bottomAnchor.constraint(equalTo: imageView.topAnchor, constant: -50),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
 }
-
